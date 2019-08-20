5328280ba2418ea7f44b47370c4c5bedc511ffab
Francisco De Assis Barbosa <assis_barbosa@A307-23L.imd.local>
2019-08-20 18:23:28 -0300

    Exercicio 2 - Parte 1

---
+++ blockchain_blocos.py
@@ -0,0 +1,41 @@
+import hashlib
+import json
+from time import time
+class Blockchain(object):
+
+    def __init__(self):
+        self.chain = []
+        self.memPool = []
+        self.createGenesisBlock()
+
+    def createGenesisBlock(self):
+        # Implemente aqui o método para gerar o bloco Genesis, invocado no construtor da classe,
+        # chamando o método createBlock() previamente implementado.
+        self.createBlock(previousHash=0)        
+    def createBlock(self, nonce=0, previousHash=None):
+        # Implemente aqui o método para retornar um bloco (formato de dicionário)
+        # Lembre que o hash do bloco anterior é o hash na verdade do CABEÇALHO do bloco anterior.
+        bloco = {
+            'index': len(self.chain)+1,
+            'timestamp': time(),
+            'nonce': 0,
+            'merkleRoot': 0,
+            'previousHash': self.generateHash(previousHash)
+        }
+        self.transactions = []
+        self.chain.append(bloco)
+        return bloco
+
+    @staticmethod
+    def generateHash(data):
+        blkSerial = json.dumps(data, sort_keys=True).encode()
+        return hashlib.sha256(blkSerial).hexdigest()
+
+    def printChain(self):
+        # Implemente aqui um método para imprimir de maneira verbosa e intuitiva o blockchain atual.
+        pass
+
+# Teste
+blockchain = Blockchain()
+for x in range(0, 3): blockchain.createBlock()
+blockchain.printChain()
